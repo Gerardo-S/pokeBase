ecfdadd98a2be9446febc431879caaaa9189cb21
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-10-05 22:05:04 -0700

    Chore: add color schemes for background and update boilerplate theme

--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,36 @@
-  
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { typographyClasses } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
-// Create a theme instance.
+// A custom theme for this app
 const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 477,
+      md: 983,
+      lg: 1200,
+      xl: 1536
+    }
+  },
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "rgba(255, 255, 255, 1)",
+      mainGradient:
+        "linear-gradient(90deg, rgba(0,0,0) 20%, rgba(0,0,0,0) 100%)",
+      navGradient:
+        "linear-gradient(90deg, #010101 0%, rgba(0,0,0,.50) 30%, rgba(0,0,0,0) 44%)"
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-
+    btnPrimary: {
+      main: "#2885F6"
+    }
   },
+  typography: {
+    subtitle1: {
+      fontSize: 18,
+      fontWeight: 700,
+      color: "#FFFFFF"
+    }
+  }
 });
 
 export default theme;
