aed61f8e4b9c9776573bcb35e3a38a49e99941eb
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-10-06 18:44:44 -0700

    Chore: add typography for home page content

--- src/theme.js
+++ src/theme.js
@@ -18,7 +18,9 @@ const theme = createTheme({
       mainGradient:
         "linear-gradient(90deg, rgba(0,0,0) 20%, rgba(0,0,0,0) 100%)",
       navGradient:
-        "linear-gradient(90deg, #010101 0%, rgba(0,0,0,.50) 30%, rgba(0,0,0,0) 44%)"
+        "linear-gradient(90deg, #010101 0%, rgba(0,0,0,.50) 30%, rgba(0,0,0,0) 44%)",
+      footerGradient:
+        "linear-gradient(90deg, rgba(0,0,0,0) 30%, rgba(0,0,0,.50) 60%,#010101 90%)"
     },
     btnPrimary: {
       main: "#2885F6"
@@ -28,6 +30,21 @@ const theme = createTheme({
     subtitle1: {
       fontSize: 18,
       fontWeight: 700,
+      color: "#FFFFFF"
+    },
+    subtitle2: {
+      fontSize: 14,
+      fontWeight: 700,
+      color: "#FFFFFF"
+    },
+    h1: {
+      fontSize: 56,
+      fontWeight: 700,
+      color: "#FFFFFF"
+    },
+    h2: {
+      fontSize: 18,
+      fontWeight: 400,
       color: "#FFFFFF"
     }
   }
