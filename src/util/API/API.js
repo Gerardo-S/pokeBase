40000df19ea88264e82ccb8707994153a14d457b
Gerardo Solis <gerardoa.solis@yahoo.com>
2021-10-27 13:51:40 -0700

    Chore: Remove comment

--- src/util/API/API.js
+++ src/util/API/API.js
@@ -23,28 +23,6 @@ async function getPokemonSpeciesDetail(pokemonId) {
     `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`
   );
 
-  //  TODO Extract only necessary information ===========================================
-  // const modifiedRes = response.map((s) => {
-  //   const {
-  //     egg_group,
-  //     evolves_from_species,
-  //     flavor_text_entries,
-  //     growth_rate,
-  //     habitat,
-  //     is_legendary,
-  //     is_mythical
-  //   } = s;
-  //   return {
-  //     eggGroup: egg_group,
-  //     evolvedFrom: evolves_from_species,
-  //     growthRate: growth_rate,
-  //     isLegendary: is_legendary,
-  //     isMythical: is_mythical,
-  //     description: flavor_text_entries,
-  //     habitat
-  //   };
-  // });
-
   if (!response.ok) throw Error(response.statusText);
   return await response.json();
 }
